44629d19b2e289c7accd1f48dea9fc5036ec9ce0
Matheus Mahl <matheusmahl@outlook.com>
2016-11-26 11:06:59 -0200

    Atualização do projeto com a Splash Screen e Tela de login.

---
+++ app/src/main/java/br/edu/pdm/cityharm/LoginActivity.java
@@ -0,0 +1,73 @@
+package br.edu.pdm.cityharm;
+
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends AppCompatActivity {
+
+  @ViewById
+  protected EditText edtLogin;
+  @ViewById
+  protected EditText edtSenha;
+  @ViewById
+  protected ImageButton btnLogin;
+  @ViewById
+  protected ImageButton btnSobre;
+
+  @ViewById
+  protected TextView txtRegistrar;
+
+  @AfterViews
+  public void inicializar() {
+    //Deixar o texto sublinhado
+    txtRegistrar.setPaintFlags(txtRegistrar.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+  }
+
+  public void abrirRegistro(View view) {
+   // Intent itRegistro = new Intent(this, CadastroActivity_.class);
+    // espera resultado da tela de cadastro de usuario
+  //  startActivityForResult(itRegistro, 100);
+  }
+
+  @Click({R.id.btnSobre, R.id.btnLogin})
+  public void onClick(View view) {
+   // DatabaseHelper dh = new DatabaseHelper(this);
+
+    switch (view.getId()) {
+      case R.id.btnLogin:
+        // recuperar valores da tela
+        String strLogin = edtLogin.getText().toString();
+        String strSenha = edtSenha.getText().toString();
+     //   Usuario u = dh.getUsuarioByLoginSenha(strLogin, strSenha);
+       // if (u != null) {
+        if (strLogin != "") {
+         // Intent it = new Intent(this, PrincipalActivity.class);
+       //   it.putExtra("usuario", u);
+        //  startActivity(it);
+          finish();
+        } else {
+          edtLogin.setText("");
+          edtSenha.setText("");
+          Toast.makeText(this, R.string.msgLoginErro, Toast.LENGTH_LONG).show();
+          edtLogin.requestFocus();
+        }
+        break;
+
+      case R.id.btnSobre:
+        finish();
+        break;
+    }
+  }
+}
